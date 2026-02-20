1e67737d263b6c6103cbed26d944a925a93a479c
Gunner Blake Carr <topdawgk.a@gmail.com>
2026-02-19 22:21:25 -0600

    Create WorkOrdersScreen.tsx

---
+++ WorkOrdersScreen.tsx
@@ -0,0 +1,9 @@
+import { Button } from 
+"react-native";
+
+// Inside your return adove Flatlist
+<Button
+  title="Add Work Order"
+  onPress={() =>
+    navigation.navigate("Create Work Order")}
+  />
